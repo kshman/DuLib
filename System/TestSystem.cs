e89581e7b88d944356d2f1d9bc3b572b66721eb3
KIM <kshman@msn.com>
2021-11-06 04:47:48 +0900

    unixtime, testsystem

---
+++ System/TestSystem.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing.Text;
+using System.Security.Principal;
+
+namespace DuLib.System
+{
+	public static class TestSystem
+	{
+		public static bool IsAdministrator
+		{
+			get
+			{
+				WindowsIdentity identity = WindowsIdentity.GetCurrent();
+				WindowsPrincipal principal = new WindowsPrincipal(identity);
+				return principal.IsInRole(WindowsBuiltInRole.Administrator);
+			}
+		}
+
+		public static bool IsFontInstalled(string fontname)
+		{
+			InstalledFontCollection collection = new InstalledFontCollection();
+			foreach (var family in collection.Families)
+			{
+				if (family.Name == fontname)
+					return true;
+			}
+			return false;
+		}
+	}
+}
