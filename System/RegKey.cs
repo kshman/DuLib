d1cd88e6f4e6c353822b258223df5c0dbf504036
KIM <kshman@msn.com>
2021-11-06 20:04:57 +0900

    추가: 레지스트리 확장자 등록

--- System/RegKey.cs
+++ System/RegKey.cs
@@ -5,9 +5,8 @@ namespace DuLib.System
 {
 	public class RegKey : IDisposable
 	{
-		private const string BaseKey = "Software";
+		private readonly string BaseKey = "Software";
 
-		private string _key;
 		private RegistryKey _rk;
 
 		public RegKey(string keyname, bool createnew = false)
@@ -17,11 +16,16 @@ namespace DuLib.System
 
 		public RegKey(string keyname, RegistryKey highkey, bool createnew = false)
 		{
-			_key = BaseKey + "\\" + keyname;
+			var key = BaseKey + "\\" + keyname;
 
-			_rk = highkey.OpenSubKey(_key, true);
+			_rk = highkey.OpenSubKey(key, true);
 			if (_rk == null && createnew)
-				_rk = highkey.CreateSubKey(_key);
+				_rk = highkey.CreateSubKey(key);
+		}
+
+		private RegKey(RegistryKey rk)
+		{
+			_rk = rk;
 		}
 
 		public void Dispose()
@@ -39,7 +43,22 @@ namespace DuLib.System
 			}
 		}
 
+		public override string ToString()
+		{
+			return _rk != null ? _rk.ToString() : "[closed]";
+		}
+
 		public bool IsOpen { get { return _rk != null; } }
+
+		public RegKey CreateKey(string keyname)
+		{
+			return _rk == null ? null : new RegKey(_rk.CreateSubKey(keyname));
+		}
+
+		public object GetValue(string name)
+		{
+			return _rk != null ? _rk.GetValue(name) : null;
+		}
 
 		public string GetString(string name, string failret = null)
 		{
@@ -54,6 +73,12 @@ namespace DuLib.System
 		public byte[] GetBytes(string name)
 		{
 			return _rk != null && _rk.GetValue(name) is byte[] value ? value : null;
+		}
+
+		public void SetValue(string name, object value)
+		{
+			if (_rk != null)
+				_rk.SetValue(name, value);
 		}
 
 		public void SetString(string name, string value)
@@ -78,6 +103,91 @@ namespace DuLib.System
 		{
 			if (_rk != null)
 				_rk.SetValue(name, value, RegistryValueKind.Binary);
+		}
+
+		public bool DeleteKey(string keyname, bool also_delete_tree = false)
+		{
+			if (_rk == null)
+				return false;
+
+			try
+			{
+				if (also_delete_tree)
+					_rk.DeleteSubKeyTree(keyname);
+				else
+					_rk.DeleteSubKey(keyname);
+				return true;
+			}
+			catch { return false; }
+		}
+
+		public bool DeleteValue(string name)
+		{
+			if (_rk == null)
+				return false;
+
+			try
+			{
+				_rk.DeleteValue(name);
+				return true;
+			}
+			catch { return false; }
+		}
+
+		//
+		public static bool RegisterExtension(string extension, string type, string description, string executepath, string friendlyname = null)
+		{
+			// (".testext", "Test.testext", "Test extension register", "c:\test.exe", "테스트프로그램")
+
+			try
+			{
+				using (var rc = new RegKey("Classes"))
+				{
+					using (var re = rc.CreateKey(extension))
+						re.SetString(null, type);
+
+					using (var rt = rc.CreateKey(type))
+					{
+						rt.SetString(null, description);
+
+						using (var rs = rt.CreateKey("shell"))
+						{
+							using (var ro = rs.CreateKey("open"))
+							{
+								if (!string.IsNullOrEmpty(friendlyname))
+									rc.SetString("FriendlyAppName", friendlyname);
+
+								using (var rn = ro.CreateKey("command"))
+								{
+									var command = string.Format("\"{0}\" \"%1\"", executepath);
+									rn.SetString(null, command);
+								}
+							}
+						}
+					}
+				}
+
+				return true;
+			}
+			catch { return false; }
+		}
+
+		//
+		public static bool UnregisterExtension(string extension, string type)
+		{
+			// (".testext", "Test.testext")
+
+			try
+			{
+				using (var rc = new RegKey("Classes"))
+				{
+					rc.DeleteKey(extension);
+					rc.DeleteKey(type, true);
+				}
+
+				return true;
+			}
+			catch { return false; }
 		}
 	}
 }
