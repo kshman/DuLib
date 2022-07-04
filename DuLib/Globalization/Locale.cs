e3a7aa3b072bc5054d0eda88bbd1ce4cc11e5ec0
kim <kshman@msn.com>
2022-07-04 19:50:36 +0900

    로캘 기능 동기화

--- DuLib/Globalization/Locale.cs
+++ DuLib/Globalization/Locale.cs
@@ -5,138 +5,152 @@ using Du.Data;
 
 namespace Du.Globalization
 {
-/// <summary>
-/// 로캘 데이터를 처리
-/// </summary>
-public static class Locale
-{
-	private static readonly LineDbV3 s_db = LineDbV3.Empty();
-	private static readonly Dictionary<string, string> s_langs = new Dictionary<string, string>();
-
 	/// <summary>
-	/// 현재 로캘을 얻는다
+	/// 로캘 데이터를 처리
 	/// </summary>
-	public static string CurrentLocale { get; private set; } = string.Empty;
-
-	/// <summary>
-	/// 로캘을 추가한다
-	/// </summary>
-	/// <param name="name"></param>
-	/// <param name="context_or_filename"></param>
-	/// <param name="isfile"></param>
-	public static void AddLocale(string name, string context_or_filename, bool isfile = false)
+	public static class Locale
 	{
-		if (!isfile)
-			s_langs[name] = context_or_filename;
-		else
+		private static readonly LineDbV3 s_db = LineDbV3.Empty();
+		private static readonly Dictionary<string, string> s_langs = new Dictionary<string, string>();
+
+		/// <summary>
+		/// 현재 로캘을 얻는다
+		/// </summary>
+		public static string CurrentLocale { get; private set; } = string.Empty;
+
+		/// <summary>
+		/// 로캘 목록을 얻는다
+		/// </summary>
+		/// <returns>로켈 목록</returns>
+		public static IEnumerable<string> GetLocaleList()
 		{
-			var s = $"FILE:{context_or_filename}";
-			s_langs[name] = s;
+			return s_langs.Keys.ToArray();
 		}
-	}
 
-	/// <summary>
-	/// 로캘을 설정
-	/// </summary>
-	/// <param name="name"></param>
-	public static bool SetLocale(string name)
-	{
-		if (name != CurrentLocale)
+		/// <summary>
+		/// 로캘을 추가한다
+		/// </summary>
+		/// <param name="name"></param>
+		/// <param name="context_or_filename"></param>
+		/// <param name="isfile"></param>
+		public static void AddLocale(string name, string context_or_filename, bool isfile = false)
 		{
-			if (!s_langs.TryGetValue(name, out var context))
+			if (!isfile)
+				s_langs[name] = context_or_filename;
+			else
+			{
+				var s = $"FILE:{context_or_filename}";
+				s_langs[name] = s;
+			}
+		}
+
+		/// <summary>
+		/// 로캘을 설정
+		/// </summary>
+		/// <param name="name"></param>
+		public static bool SetLocale(string name)
+		{
+			if (name != CurrentLocale)
+			{
+				if (!s_langs.TryGetValue(name, out var context))
+					return false;
+
+				InternalSetLocale(name, context);
+			}
+
+			return true;
+		}
+
+		/// <summary>
+		/// 기본 로캘
+		/// </summary>
+		/// <returns></returns>
+		public static bool SetDefaultLocale()
+		{
+			if (s_langs.Count == 0)
 				return false;
 
-			InternalSetLocale(name, context);
+			var first = s_langs.First();
+			if (first.Key != CurrentLocale)
+				InternalSetLocale(first.Key, first.Value);
+
+			return true;
 		}
 
-		return true;
-	}
-
-	/// <summary>
-	/// 기본 로캘
-	/// </summary>
-	/// <returns></returns>
-	public static bool SetDefaultLocale()
-	{
-		if (s_langs.Count == 0)
-			return false;
-
-		var first = s_langs.First();
-		if (first.Key != CurrentLocale)
-			InternalSetLocale(first.Key, first.Value);
-
-		return true;
-	}
-
-	//
-	private static void InternalSetLocale(string name, string context)
-	{
-		if (context.StartsWith("FILE:"))
+		//
+		private static void InternalSetLocale(string name, string context)
 		{
-			// 이건 파일
-			var filename = context.Substring(5);
-			s_db.AddFromFile(filename, Encoding.UTF8, true);
+			if (context.StartsWith("FILE:"))
+			{
+				// 이건 파일
+				var filename = context.Substring(5);
+				s_db.AddFromFile(filename, Encoding.UTF8, true);
+			}
+			else
+			{
+				// 이건 컨텍스트
+				s_db.AddFromContext(context, true);
+			}
+
+			CurrentLocale = name;
 		}
-		else
+
+		/// <summary>
+		/// 로캘이 있나 확인
+		/// </summary>
+		/// <param name="name"></param>
+		/// <returns></returns>
+		public static bool HasLocale(string name)
 		{
-			// 이건 컨텍스트
-			s_db.AddFromContext(context, true);
+			return s_langs.ContainsKey(name);
 		}
 
-		CurrentLocale = name;
-	}
+		/// <summary>
+		/// 값 얻기
+		/// </summary>
+		/// <param name="key">키</param>
+		/// <returns>얻은 값</returns>
+		public static string Text(string key)
+		{
+			return s_db.Try(key, out string v) ? v : $"<{key}>";
+		}
 
-	/// <summary>
-	/// 로캘이 있나 확인
-	/// </summary>
-	/// <param name="name"></param>
-	/// <returns></returns>
-	public static bool HasLocale(string name)
-	{
-		return s_langs.ContainsKey(name);
-	}
+		/// <summary>
+		/// 값 얻고 문자열 포맷
+		/// </summary>
+		/// <param name="key">키</param>
+		/// <param name="prms">인수들</param>
+		/// <returns>만들어진 값</returns>
+		public static string Text(string key, params object[] prms)
+		{
+			return s_db.Try(key, out string v) ? string.Format(v, prms) : $"<{key}>";
+		}
 
-	/// <summary>
-	/// 값 얻기
-	/// </summary>
-	/// <param name="key">키</param>
-	/// <returns>얻은 값</returns>
-	public static string Text(string key)
-	{
-		return s_db.Try(key, out string v) ? v : $"<{key}>";
-	}
+		/// <summary>
+		/// 값 얻기
+		/// </summary>
+		/// <param name="key">키</param>
+		/// <returns>얻은 값</returns>
+		public static string Text(int key)
+		{
+			return s_db.Try(key, out string v) ? v : $"{{{key}}}";
+		}
 
-	/// <summary>
-	/// 값 얻고 문자열 포맷
-	/// </summary>
-	/// <param name="key">키</param>
-	/// <param name="prms">인수들</param>
-	/// <returns>만들어진 값</returns>
-	public static string Text(string key, params object[] prms)
-	{
-		return s_db.Try(key, out string v) ? string.Format(v, prms) : $"<{key}>";
-	}
-
-	/// <summary>
-	/// 값 얻기
-	/// </summary>
-	/// <param name="key">키</param>
-	/// <returns>얻은 값</returns>
-	public static string Text(int key)
-	{
-		return s_db.Try(key, out string v) ? v : $"{{{key}}}";
-	}
-
-	/// <summary>
-	/// 값 얻고 문자열 포맷
-	/// </summary>
-	/// <param name="key">키</param>
-	/// <param name="prms">인수들</param>
-	/// <returns>만들어진 값</returns>
-	public static string Text(int key, params object[] prms)
-	{
-		return s_db.Try(key, out string v) ? string.Format(v, prms) : $"{{{key}}}";
+		/// <summary>
+		/// 값 얻고 문자열 포맷
+		/// </summary>
+		/// <param name="key">키</param>
+		/// <param name="prms">인수들</param>
+		/// <returns>만들어진 값</returns>
+		public static string Text(int key, params object[] prms)
+		{
+			return s_db.Try(key, out string v) ? string.Format(v, prms) : $"{{{key}}}";
+		}
 	}
 }
 
-}
+
+// 폼데이터 일괄 변환
+// Text = "\{(\d{4})\}";
+// Text = "\{(\d{1,4})\}";
+// Text = Du.Globalization.Locale.Text($1);
